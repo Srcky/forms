7d866214dd27803b028a4bdbe2c4a5aec6ec54c6
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-09-30 13:53:45 +0200

    Auto component

--- src/app/auto-form/auto-form.component.ts
+++ src/app/auto-form/auto-form.component.ts
@@ -18,16 +18,57 @@ export class AutoFormComponent implements OnInit {
 
   ngOnInit() {
     this.defaultForm = this.fb.group({
-      destination: ['', [Validators.required]],
-      startDate: ['', [Validators.required, CustomFormValidators.compareToCurrentDate('shouldBeGreater')]],
-      endDate: ['', [Validators.required, CustomFormValidators.compareToCurrentDate('shouldBeGreater')]],
-      durationInDays: ['', [Validators.required]],
-      tripCost: ['', [Validators.required, Validators.min(0), Validators.max(1000000), Validators.pattern('^[0-9]*$')]],
-      numberOfTravelers: ['', Validators.required],
-      otherTravelers: this.fb.array([
-        // this.makeNew()
+      general: this.fb.group({
+        coverageStartDate: ['', [Validators.required, CustomFormValidators.compareToCurrentDate('shouldBeGreater')]],
+        paymentFrequency: ['', Validators.required]
+      }),
+      vehicle: this.fb.group({
+        vehicleMake: ['', Validators.required],
+        vehicleModel: ['', Validators.required],
+        vehicleType: ['', Validators.required],
+        vehicleYear: ['', Validators.required],
+        vehicleAnnualMileage: ['', [Validators.required, Validators.max(100000), Validators.pattern('^[0-9]*$')]],
+        vehicleValue: ['', [Validators.required, Validators.min(3000), Validators.max(1000000), Validators.pattern('^[0-9]*$')]],
+        vehicleUsage: ['', Validators.required],
+        vehiclePurchaseDate: ['', [Validators.required, CustomFormValidators.compareToCurrentDate('shouldBeLess')]],
+        vehicleOwnerPostalCode: ['', Validators.required]
+      }),
+      mainDriver: this.fb.group({
+        driverDob: ['', [Validators.required, CustomFormValidators.dateOfBirthValidator(18)]],
+        driverGender: ['', Validators.required],
+        driverMaritalStatus: ['', Validators.required],
+        driverCategory: ['', Validators.required],
+        driverLicenceDate: ['', Validators.required, CustomFormValidators.compareToCurrentDate('shouldBeLess')]
+      }),
+      additionalDrivers: this.fb.group({
+        numberOfDrivers: ['', Validators.required],
+      }),
+      additionalDriver: this.fb.array([
+        this.makeNew()
       ])
     });
   }
-
+  makeNew(): FormGroup {
+    return this.fb.group({
+      driverDob: ['', [Validators.required, CustomFormValidators.dateOfBirthValidator(18)]],
+      driverGender: ['', Validators.required],
+      driverMaritalStatus: ['', Validators.required],
+      driverCategory: ['', Validators.required],
+      driverLicenceDate: ['', Validators.required, CustomFormValidators.compareToCurrentDate('shouldBeLess')]
+    });
+  }
+  get formArr() {
+    return this.defaultForm.get('otherTravelers') as FormArray;
+  }
+  toggleFormControls(qty: number): void {
+    if (qty > this.formArr.length) {
+      for (let i = this.formArr.length; i < qty; i++) {
+        this.formArr.push(this.makeNew());
+      }
+    } else {
+      while (this.formArr.length > qty) {
+        this.formArr.removeAt(0);
+      }
+    }
+  }
 }
