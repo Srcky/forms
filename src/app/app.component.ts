d8988a6881bc8bbd7e7f9a3fb6e58a61709ca38d
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-05-14 21:11:22 +0200

    first checkbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.Native
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class AppComponent {
   title = 'forms-playground';
--- src/app/travel-form/travel-form.component.ts
+++ src/app/travel-form/travel-form.component.ts
@@ -5,13 +5,10 @@ import { CustomFormValidators } from '../util/validators/custom-form-validators'
 @Component({
   selector: 'app-travel-form',
   templateUrl: './travel-form.component.html',
-  styleUrls: ['./travel-form.component.scss']
+  styleUrls: ['./travel-form.component.scss'],
 })
 export class TravelFormComponent implements OnInit {
-
-  constructor(
-    private fb: FormBuilder
-  ) { }
+  constructor(private fb: FormBuilder) {}
 
   defaultForm: FormGroup;
 
@@ -19,34 +16,66 @@ export class TravelFormComponent implements OnInit {
     'Europe',
     'Australia and New Zealand',
     'Worldwide(excluding USA, Canada and the Caribbean)',
-    'Worldwide(including USA, Canada and the Caribbean)'
+    'Worldwide(including USA, Canada and the Caribbean)',
   ];
+  materials: Array<string> = ['brick', 'wood', 'concrete'];
+  selectedMaterials: Array<string>;
   noOfTravelers = 10;
-  travelers = Array(this.noOfTravelers).fill(null).map((k, i) => (i + 1));
-  // travelers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  travelers = Array(this.noOfTravelers)
+    .fill(null)
+    .map((k, i) => i + 1);
 
   ngOnInit() {
     this.defaultForm = this.fb.group({
-      destination: ['', [Validators.required]],
-      startDate: ['', [Validators.required, CustomFormValidators.compareToCurrentDate('shouldBeGreater')]],
-      endDate: ['', [Validators.required, CustomFormValidators.compareToCurrentDate('shouldBeGreater')]],
-      durationInDays: ['', [Validators.required]],
-      tripCost: ['', [Validators.required, Validators.min(0), Validators.max(1000000), Validators.pattern('^[0-9]*$')]],
-      numberOfTravelers: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
-      otherTravelers: this.fb.array([
-        this.makeNew()
-      ])
+      constructionMaterial: this.makeMaterial(),
+      numberOfTravelers: [
+        '',
+        [Validators.required, Validators.pattern('^[0-9]*$')],
+      ],
+      otherTravelers: this.fb.array([this.makeNew()]),
+      selMat: this.fb.array([this.makeMaterial()]),
     });
   }
 
   onSubmit() {
-    // console.log(this.defaultForm);
+    // const selMat = this.selectedMaterials;
+    // console.log({ ...this.defaultForm.value, selMat });
+    console.log(this.defaultForm);
   }
-  makeNew(): FormGroup {
-    return this.fb.group({
-      ageOfTraveler: ['', [Validators.required, Validators.min(1), Validators.max(150), Validators.pattern('^[0-9]*$')]]
+
+  makeMaterial() {
+    const arr = this.materials.map((_) => {
+      return this.fb.control(false);
+    });
+    return this.fb.array(arr);
+  }
+
+  getSelectedMaterial() {
+    this.selectedMaterials = [];
+    this.materialArr.controls.forEach((ctrl, i) => {
+      if (ctrl.value) {
+        this.selectedMaterials.push(this.materials[i]);
+      }
     });
   }
+
+  makeNew(): FormGroup {
+    return this.fb.group({
+      ageOfTraveler: [
+        '',
+        [
+          Validators.required,
+          Validators.min(1),
+          Validators.max(150),
+          Validators.pattern('^[0-9]*$'),
+        ],
+      ],
+    });
+  }
+  get materialArr() {
+    return this.defaultForm.get('constructionMaterial') as FormArray;
+  }
+
   get formArr() {
     return this.defaultForm.get('otherTravelers') as FormArray;
   }
@@ -61,5 +90,4 @@ export class TravelFormComponent implements OnInit {
       }
     }
   }
-
 }
